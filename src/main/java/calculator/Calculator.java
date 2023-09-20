fc04f53025b4957c50f9eb447a45db9b8c201989
Tim <timfill2012@gmail.com>
2023-09-20 17:50:46 +0300

    second commit with work version

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,88 +3,47 @@ package calculator;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
-    /*
-    Решил сделать два класса:
-     Первый собственно калькулятор состоящий из двух методов - main и метода математических
-    операций с арабскими цифрами;
-     Второй вспомогательный, вложенный - конвертирующий римские цифры в арабские и наоборот;
-
-     Программа выполняет одну операцию, после чего завершается. Так как указаний на этот счет в требованиях не было,
-     я ничего с этим делать и не стал.
-     */
-
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String expression = reader.readLine();                                         // получаем входные данные
             String[] splitString = expression.split(" ");
-            List<String> romeNumbers = new ArrayList<>();
-            romeNumbers.add("I");
-            romeNumbers.add("II");
-            romeNumbers.add("III");
-            romeNumbers.add("IV");
-            romeNumbers.add("V");
-            romeNumbers.add("VI");
-            romeNumbers.add("VII");
-            romeNumbers.add("VIII");
-            romeNumbers.add("IX");
-            romeNumbers.add("X");
+            String operation = splitString[1];
+            var number1 = splitString[0];
+            var number2 = splitString[2];
+            String[] romeNum = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+            List<String> romeNumbers = new ArrayList<>(Arrays.asList(romeNum));
             List<String> arabNumbers = new ArrayList<>();
             for (int i = 0; i < 11; i++) arabNumbers.add(String.valueOf(i));
-            int firstRomeNumber;
-            int secondRomeNumber;
 
-
-
-
-             if (romeNumbers.contains(splitString[0]) && romeNumbers.contains(splitString[2])) {
-                firstRomeNumber = NumeralConverter.fromRomanNumeral(splitString[0]);
-                secondRomeNumber = NumeralConverter.fromRomanNumeral(splitString[2]);
-                String[] romeValues = {String.valueOf(firstRomeNumber),splitString[1], String.valueOf(secondRomeNumber)};
-                int arabicSumma = arabicOperation(romeValues);
-                if ((arabicSumma < 1)) {
-                    System.out.println("throws Exception");
-                    throw new Exception();
-                    // System.out.println("throw Exception // т.к. в римской системе нет отрицательных чисел");
-                }
-                String answer = NumeralConverter.toRomanNumeral(arabicSumma);
+            if (romeNumbers.contains(number1) && romeNumbers.contains(number2) && (splitString.length < 4)) {
+                int num1 = NumeralConverter.fromRomanNumeral(splitString[0]);
+                int num2 = NumeralConverter.fromRomanNumeral(splitString[2]);
+                int result = calc(num1, num2, operation);
+                if ((result < 1)) throw new Exception();
+                String answer = NumeralConverter.toRomanNumeral(result);
                 System.out.println(answer);
-            } else if (arabNumbers.contains(splitString[0]) && arabNumbers.contains(splitString[2]) && (splitString.length < 4)) {
-                System.out.println(arabicOperation(splitString));
+            } else if (arabNumbers.contains(number1) && arabNumbers.contains(number2) && (splitString.length < 4)) {
+                System.out.println(calc(Integer.parseInt(splitString[0]), Integer.parseInt(splitString[2]), operation));
             } else {
-                System.out.println("throws Exception");
+                throw new Exception();
             }
         } catch (Exception e) {
-            e.printStackTrace();
             System.out.println("throws Exception");
         }
     }
 
-    public static Integer arabicOperation(String[] string) throws Exception {   // метод математических операций
-        int result = 0;
-        int first = Integer.parseInt(string[0]);
-        int second = Integer.parseInt(string[2]);
-
-        switch (string[1]) {
-            case "+":
-                result = first + second;
-                break;
-            case "-":
-                result = first - second;
-                break;
-            case "*":
-                result = first * second;
-                break;
-            case "/":
-                result = first / second;
-                break;
-            default:
-                System.out.println("throws Exception");
-                throw new Exception();
-        }
-        return result;
+    public static int calc(int num1, int num2, String operation) throws Exception {   // метод математических операций
+        return switch (operation) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new Exception();
+        };
     }
 
     static class NumeralConverter {
